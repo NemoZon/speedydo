ba442909dc95fd6139a87a539e1c4d9c51c64bdb
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2025-10-01 21:39:09 +0200

    deps(api): add express-rate-limit

--- apps/api/src/middlewares/apikey.middleware.ts
+++ apps/api/src/middlewares/apikey.middleware.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Response, Request } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { API_KEY } from '../consts/settings';
 
 export function apiKey(req: Request, res: Response, next: NextFunction) {
